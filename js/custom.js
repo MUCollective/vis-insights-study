426581411942ff13fb65df8d674af21fec8af14b
abhsarma <abhsarma@umich.edu>
2020-03-11 14:47:21 -0500

    new interface

--- js/custom.js
+++ js/custom.js
@@ -10,11 +10,9 @@ function shuffle(array) {
 	return array;
 }
 
-
 var n = 30;
 var b = [...Array(n).keys()].map( i => ++i );
 var trial_order = b.map( i => (i * mod) % (n+1));
-var img_order = shuffle([...Array(10).keys()].map( i => ++i ));
 var selected = [];
 
 page_setup = function(count, prev, payout) {
@@ -25,24 +23,51 @@ page_setup = function(count, prev, payout) {
 
 	trial_idx = count % n;
 	cond_idx = Math.floor(count / n);
-	alpha = parseInt(incentives[cond_idx])/100;
+	if (charts[cond_idx] == "m") {n_regions = 12} else {n_regions = 8};
+
+	var img_order = shuffle([...Array(n_regions).keys()].map( i => ++i ));
+
+	console.log(count, charts, n_regions, img_order);
+	alpha = 0.05; // parseInt(incentives[cond_idx])/100;
 
 	$("span#fp-val").text("" + Math.round(((1-alpha)/alpha) * 20) + " points");
 	// the payoff matrix images/payoff_5.png
-	$("img#payoff-img").attr("src", "images/payoff_" + incentives[cond_idx] + ".png");
+	$("img#payoff-img").attr("src", "images/payoff_" + (alpha*100) + ".png"); // incentives[cond_idx] + ".png");
 
-	url = "https://raw.githubusercontent.com/MUCollective/vis-insight-reliability/stimuli/plots/"+ conditions[cond_idx] +"/profits-t"+ trial_order[trial_idx] + "-p";
+	if (n_regions == 12){
+		url = "https://raw.githubusercontent.com/MUCollective/vis-insight-reliability/stimuli/plots/"+ conditions[cond_idx] +"/12-regions/profits-t"+ trial_order[trial_idx] + "-p";
+		$("div#opt-1").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[0] + ".png");
+		$("div#opt-2").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[1] + ".png");
+		$("div#opt-3").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[2] + ".png");
+		$("div#opt-4").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[3] + ".png");
+		$("div#opt-5").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[4] + ".png");
+		$("div#opt-6").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[5] + ".png");
+		$("div#opt-7").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[6] + ".png");
+		$("div#opt-8").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[7] + ".png");
+		$("div#opt-9").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[8] + ".png");
+		$("div#opt-10").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[9] + ".png");
+		$("div#opt-11").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[10] + ".png");
+		$("div#opt-12").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[11] + ".png");
+	} else {
+		url = "https://raw.githubusercontent.com/MUCollective/vis-insight-reliability/stimuli/plots/"+ conditions[cond_idx] +"/8-regions/profits-t"+ trial_order[trial_idx] + "-p";
+		$("div#opt-1").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[0] + ".png");
+		$("div#opt-2").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[1] + ".png");
+		$("div#opt-3").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[2] + ".png");
+		$("div#opt-4").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[3] + ".png");
+		$("div#opt-5").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[4] + ".png");
+		$("div#opt-6").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[5] + ".png");
+		$("div#opt-7").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[6] + ".png");
+		$("div#opt-8").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[7] + ".png");
 
-	$("div#opt-1").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[0] + ".png");
-	$("div#opt-2").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[1] + ".png");
-	$("div#opt-3").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[2] + ".png");
-	$("div#opt-4").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[3] + ".png");
-	$("div#opt-5").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[4] + ".png");
-	$("div#opt-6").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[5] + ".png");
-	$("div#opt-7").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[6] + ".png");
-	$("div#opt-8").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[7] + ".png");
-	$("div#opt-9").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[8] + ".png");
-	$("div#opt-10").append('<img class = "question-graphic">').children().attr("src", url + "0.5-" + img_order[9] + ".png");
+		$("div#opt-1").parent().addClass("col-lg-offset-2");
+		$("div#opt-5").parent().addClass("col-lg-offset-2");
+
+
+		$("div#opt-9").hide();
+		$("div#opt-10").hide();
+		$("div#opt-11").hide();
+		$("div#opt-12").hide();
+	}
 
 	var cond_idx = Math.floor(count / n);
 	var trial_idx = count  % n;
@@ -91,6 +116,7 @@ $(".question-container").click(function () {
 		var idx = selected.indexOf(id);
 		if (idx !== -1) selected.splice(idx, 1);
 	}
+	console.log(selected);
 });
 
 // send response to DB
@@ -121,8 +147,8 @@ function update_trial_page(c, responses, time) {
 	var prev_t_idx = c % n;
 
 	var submit_response_url = 'https://w8vewq61bf.execute-api.us-east-2.amazonaws.com/prod/recordResponses?PROLIFIC_PID='+
-	prolific_PID+'&SESSION_ID='+session_ID+'&responses='+responses+'&vis='+conditions[prev_c_idx]+
-	'&alpha='+incentives[prev_c_idx]+'&trial='+trial_order[prev_t_idx]+'&t='+time+'&trials_count='+c+'';
+	prolific_PID+'&SESSION_ID='+session_ID+'&responses='+responses+'&vis='+conditions[prev_c_idx]+'&nregions='+n_regions+
+	'&trial='+trial_order[prev_t_idx]+'&t='+time+'&trials_count='+c+'';
 
 	$.get(submit_response_url).done(function (data) {
 		to_fwd = window.btoa( [data['count'], Object.values(data['prev']), data['payout']].flat() );
@@ -137,12 +163,12 @@ function update_trial_page(c, responses, time) {
 			})
 		} else {
 			if (((c + 1) % n) == 0) {
-				window.location.href = 'intro.html?PROLIFIC_PID='+prolific_PID+'&SESSION_ID='+session_ID+'&modulo='+mod+'&ret='+to_fwd+'&vis='+conditions+'&alpha='+incentives+'';
+				window.location.href = 'intro.html?PROLIFIC_PID='+prolific_PID+'&SESSION_ID='+session_ID+'&modulo='+mod+'&ret='+to_fwd+'&vis='+conditions+'&ch='+charts+'';
 			} else {
 				if ((c % (n - 3)) == 21){ // three attention checks at 21 48 75
-					window.location.href = 'attention.html?PROLIFIC_PID='+prolific_PID+'&SESSION_ID='+session_ID+'&modulo='+mod+'&ret='+to_fwd+'&vis='+conditions+'&alpha='+incentives+'';
+					window.location.href = 'attention.html?PROLIFIC_PID='+prolific_PID+'&SESSION_ID='+session_ID+'&modulo='+mod+'&ret='+to_fwd+'&vis='+conditions+'&ch='+charts+'';
 				} else {
-					window.location.href = 'trial.html?PROLIFIC_PID='+prolific_PID+'&SESSION_ID='+session_ID+'&modulo='+mod+'&ret='+to_fwd+'&vis='+conditions+'&alpha='+incentives+'';
+					window.location.href = 'trial.html?PROLIFIC_PID='+prolific_PID+'&SESSION_ID='+session_ID+'&modulo='+mod+'&ret='+to_fwd+'&vis='+conditions+'&ch='+charts+'';
 				}
 			}
 		}
